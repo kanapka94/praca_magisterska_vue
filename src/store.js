d4d87df77fb0d53790bd89d53ef359a9c2845a1a
Adam Konopka <a.konopka@p-programisci.pl>
2019-08-24 10:52:13 +0200

    Add vuex

--- src/store.js
+++ src/store.js
@@ -5,12 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    drinks: [],
+    loading: false
   },
   mutations: {
-
+    SET_DRINKS (state, payload) {
+      state.drinks = [...payload]
+    },
+    SET_LOADING (state, payload) {
+      state.loading = payload
+    }
   },
   actions: {
-
+    async fetchDrinks ({ commit }, payload) {
+      commit('SET_LOADING', true)
+      let response = []
+      if (payload.value && payload.criteria) {
+        let service = this.$service.filter[payload.criteria]
+        if (payload.criteria === 'id') {
+          service = this.$service.drinks.getDrinkById
+        } else if (payload.criteria === 'name') {
+          service = this.$service.drinks.getDrinkByName
+        }
+        response = await service(payload.value)
+      }
+      commit('SET_DRINKS', response.drinks || [])
+      setTimeout(() => {
+        commit('SET_LOADING', false)
+      }, 250)
+    }
   }
 })
