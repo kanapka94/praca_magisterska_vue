530ffe9869a5674531a671d39b8c312c901d0c57
Adam Konopka <a.konopka@p-programisci.pl>
2019-08-24 11:39:39 +0200

    Add menu and footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <the-menu />
+    <router-view />
+    <the-footer />
   </div>
 </template>
 
+<script>
+import TheMenu from '@/components/general/TheMenu/index'
+import TheFooter from '@/components/general/TheFooter'
+
+export default {
+  components: {
+    TheFooter,
+    TheMenu
+  }
+}
+</script>
+
 <style lang="scss">
 @import "@/assets/scss/_main.scss";
-@import "@/assets/scss/_transitions.scss"
+@import "@/assets/scss/_transitions.scss";
 </style>
