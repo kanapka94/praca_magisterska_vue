d02769c98e8a12d2c450fffeafd7fe95648cc214
Adam Konopka <a.konopka@p-programisci.pl>
2019-08-24 12:54:49 +0200

    Add search page to routes

---
+++ src/views/Search.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="page">
+    <h1 class="title">
+      Drinks found
+      <span class="number">{{ drinksNumber }}</span>
+    </h1>
+    <search-bar
+      v-model="form.text"
+      :placeholder="placeholder"
+      @search="handleSearch"
+    />
+    <search-results :drinks="drinks"/>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import SearchBar from '@/components/home/SearchBar/index'
+import SearchResults from '@/components/search/SearchResults/index'
+
+const PLACEHOLDERS = {
+  name: 'Type name of drink',
+  id: 'Type drink id',
+  ingredient: 'Type name of ingredient',
+  glass: 'Type name of glass',
+  alcohol: 'Type name of the alcohol saturation',
+  category: 'Type category name'
+}
+
+export default {
+  name: 'PageSearch',
+  head () {
+    return {
+      title: 'Search cocktails - FunnyDrinks',
+      meta: [
+        { hid: 'description', name: 'description', content: 'Search for the best drinks and choose the ones that suit you best' },
+        { hid: 'og:title', property: 'og:title', content: 'Search cocktails - FunnyDrinks' },
+        { hid: 'og:description', property: 'og:description', content: 'Search for the best drinks and choose the ones that suit you best' }
+      ]
+    }
+  },
+  components: { SearchResults, SearchBar },
+  data () {
+    return {
+      form: {
+        text: '',
+        criteria: 'name'
+      }
+    }
+  },
+  computed: {
+    ...mapState({
+      drinks: state => state.drinks
+    }),
+    drinksNumber () {
+      return this.$store.state.drinks.length
+    },
+    placeholder () {
+      return PLACEHOLDERS[this.form.criteria || 'name']
+    }
+  },
+  async asyncData ({ query, store }) {
+    const payload = {
+      value: query.query,
+      criteria: query.criteria
+    }
+    await store.dispatch('fetchDrinks', payload)
+    store.commit('SET_LOADING', false)
+  },
+  methods: {
+    async handleSearch () {
+      const query = {
+        query: this.form.text,
+        criteria: this.form.criteria
+      }
+      this.$router.replace({ path: '/search', query })
+      await this.$store.dispatch('fetchDrinks', {
+        value: query.query,
+        criteria: query.criteria
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .title {
+    @include font-primary(32px);
+  }
+
+  .number {
+    color: $pink;
+  }
+
+  .subtitle {
+    @include font-primary;
+    margin-top: 20px;
+  }
+
+  .radio-list {
+    margin-top: 20px;
+  }
+</style>
