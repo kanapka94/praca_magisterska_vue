8cfbe873d3ebe2afbb4a44974e26486e148aef69
Adam Konopka <a.konopka@p-programisci.pl>
2019-08-26 19:34:11 +0200

    Add vue analytics and vue gtm

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import store from './store'
 
 import service from '@/service'
 import VueMeta from 'vue-meta'
+import VueAnalytics from 'vue-analytics'
+import VueGtm from 'vue-gtm'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-146400602-2',
+  router
+})
+
+Vue.use(VueGtm, {
+  id: 'GTM-NS7PRQB',
+  enabled: true,
+  loadScript: true,
+  vueRouter: router
+})
 
 Vue.use(VueMeta, {
   refreshOnceOnNavigation: true
